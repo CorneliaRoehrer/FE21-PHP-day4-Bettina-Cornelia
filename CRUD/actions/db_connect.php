3b9d9fd0d76eb98e99bae0c54c750ae0c51d9c78
Bettina Koerbler <bettina.koerbler@gmail.com>
2021-04-22 13:18:22 +0200

    created db_connect content

---
+++ CRUD/actions/db_connect.php
@@ -0,0 +1,16 @@
+<?php 
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sewing_db";
+
+$connect = mysqli_connect($hostname, $username, $password, $dbname);
+
+if($connect ->connect_error) {
+    die("Connection failed: ".$connect->connect_error);
+} else {
+    echo "Successfully connected";
+}
+
+?>
